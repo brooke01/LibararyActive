ec21f2d9ae337e588af48d5dff57a1edef44ffd4
brooke <hd5870@hotmail.com>
2014-05-13 14:44:26 +0800

    起始專案

---
+++ 擷取圖書館活動資訊/擷取圖書館活動資訊/Form1.Designer.vb
@@ -0,0 +1,98 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form 覆寫 Dispose 以清除元件清單。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    '為 Windows Form 設計工具的必要項
+    Private components As System.ComponentModel.IContainer
+
+    '注意:  以下為 Windows Form 設計工具所需的程序
+    '可以使用 Windows Form 設計工具進行修改。
+    '請不要使用程式碼編輯器進行修改。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnActiveLoad = New System.Windows.Forms.Button()
+        Me.dgvActive = New System.Windows.Forms.DataGridView()
+        Me.TableLayoutPanel1 = New System.Windows.Forms.TableLayoutPanel()
+        Me.txtSourceXML = New System.Windows.Forms.TextBox()
+        CType(Me.dgvActive, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.TableLayoutPanel1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'btnActiveLoad
+        '
+        Me.btnActiveLoad.Location = New System.Drawing.Point(12, 12)
+        Me.btnActiveLoad.Name = "btnActiveLoad"
+        Me.btnActiveLoad.Size = New System.Drawing.Size(75, 23)
+        Me.btnActiveLoad.TabIndex = 0
+        Me.btnActiveLoad.Text = "讀取活動"
+        Me.btnActiveLoad.UseVisualStyleBackColor = True
+        '
+        'dgvActive
+        '
+        Me.dgvActive.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dgvActive.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.dgvActive.Location = New System.Drawing.Point(3, 3)
+        Me.dgvActive.Name = "dgvActive"
+        Me.dgvActive.RowTemplate.Height = 24
+        Me.dgvActive.Size = New System.Drawing.Size(417, 203)
+        Me.dgvActive.TabIndex = 1
+        '
+        'TableLayoutPanel1
+        '
+        Me.TableLayoutPanel1.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.TableLayoutPanel1.ColumnCount = 1
+        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50.0!))
+        Me.TableLayoutPanel1.Controls.Add(Me.dgvActive, 0, 0)
+        Me.TableLayoutPanel1.Location = New System.Drawing.Point(12, 41)
+        Me.TableLayoutPanel1.Name = "TableLayoutPanel1"
+        Me.TableLayoutPanel1.RowCount = 1
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50.0!))
+        Me.TableLayoutPanel1.Size = New System.Drawing.Size(423, 209)
+        Me.TableLayoutPanel1.TabIndex = 2
+        '
+        'txtSourceXML
+        '
+        Me.txtSourceXML.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.txtSourceXML.Location = New System.Drawing.Point(94, 13)
+        Me.txtSourceXML.Name = "txtSourceXML"
+        Me.txtSourceXML.Size = New System.Drawing.Size(341, 22)
+        Me.txtSourceXML.TabIndex = 3
+        Me.txtSourceXML.Text = "http://www.tphcc.gov.tw/MainPortal/htmlcnt/rss/ActvInfo"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(447, 262)
+        Me.Controls.Add(Me.txtSourceXML)
+        Me.Controls.Add(Me.TableLayoutPanel1)
+        Me.Controls.Add(Me.btnActiveLoad)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.dgvActive, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.TableLayoutPanel1.ResumeLayout(False)
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents btnActiveLoad As System.Windows.Forms.Button
+    Friend WithEvents dgvActive As System.Windows.Forms.DataGridView
+    Friend WithEvents TableLayoutPanel1 As System.Windows.Forms.TableLayoutPanel
+    Friend WithEvents txtSourceXML As System.Windows.Forms.TextBox
+
+End Class
---
+++ 擷取圖書館活動資訊/擷取圖書館活動資訊/Form1.vb
@@ -0,0 +1,49 @@
+﻿Imports System.Xml
+
+Public Class Form1
+
+    Private Sub btnActiveLoad_Click(sender As Object, e As EventArgs) Handles btnActiveLoad.Click
+        Try
+            '1讀取XML來源
+            Dim doc As New XmlDocument
+            doc.Load(txtSourceXML.Text)
+
+            '2掃描XML所有項目並儲存
+            Dim root As XmlElement = doc.DocumentElement '先生出一個root
+            '*取得第一層所有節點
+            Dim singleItemNode As XmlNode = root.SelectSingleNode("channel/item")
+            '3將項目顯示於dgv
+            Dim dt As New DataTable
+            For i = 0 To singleItemNode.ChildNodes.Count - 1
+                dt.Columns.Add(singleItemNode.ChildNodes.Item(i).Name)
+            Next
+
+            '版本一
+            Dim nodes As XmlNode = root.SelectSingleNode("channel") '取channel底下所有的節點包括非item '左括號就算是一個Node
+            For j = 0 To nodes.ChildNodes.Count - 1 'item個數為277
+                Dim dr As DataRow
+                dr = dt.NewRow 'item裡的TAG數為4
+                For k = 0 To nodes.ChildNodes.Item(j).ChildNodes.Count - 1
+                    dr(k) = nodes.ChildNodes.Item(j).ChildNodes.Item(k).InnerText
+                Next
+                dt.Rows.Add(dr)
+            Next
+
+            '版本二
+            'Dim nodes As XmlNodeList = root.SelectNodes("channel/item") '取channel底下所有的item節點
+            'For j = 0 To nodes.Count - 1 'item個數為277
+            '    Dim dr As DataRow = Nothing
+            '    dr = dt.NewRow
+            '    For k = 0 To nodes.Item(j).ChildNodes.Count - 1 'item裡的TAG數為4
+            '        dr(k) = nodes.Item(j).ChildNodes.Item(k).InnerXml
+            '    Next
+            '    dt.Rows.Add(dr)
+            'Next
+
+            dgvActive.DataSource = dt
+        Catch ex As Exception
+            MsgBox(ex.ToString)
+        End Try
+
+    End Sub
+End Class
